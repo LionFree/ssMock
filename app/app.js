0d25d17253dff5ad1afdef8548a7fe099d63828c
Curt Kaler <lionfree@earthlink.net>
2016-06-13 12:51:01 -0700

    Put module bootstrapping into app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-/**
- * Created by CKR1 on 6/13/2016.
- */
-var app = angular.module('ssMock', ['ui.bootstrap','ngAnimate']);
+(function(){
+    angular.module('ssMock', ['ui.bootstrap','ngAnimate']);
+})();
+
